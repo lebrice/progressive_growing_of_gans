70874a4c33046b2d8523a1a9e8145979883ff53c
Fabrice Normandin <fabrice.normandin@gmail.com>
2019-04-19 18:14:45 -0400

    trying to import the setup function

--- test.sh
+++ test.sh
@@ -4,5 +4,5 @@
 #SBATCH --mem=16G
 #SBATCH --time=1-00:00
 cd ~/IFT6085/progressive_growing_of_gans
-source ./train.sh 
-train LINEAR 10
+. ./train.sh
+python ./train.py --run-name TEST --blur-schedule LINEAR --train-k-images 10
--- train.sh
+++ train.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-train(){
-    cd ~/IFT6085/progressive_growing_of_gans
+cd ~/IFT6085/progressive_growing_of_gans
 
-    source ~/miniconda3/bin/activate
-    conda activate tensorflow1
+source ~/miniconda3/bin/activate
+conda activate tensorflow1
 
-    git checkout master
-    git pull
+git checkout master
+git pull
 
-    cp -r --no-clobber datasets /Tmp/pichetre/ -v
+cp -r --no-clobber datasets /Tmp/pichetre/ -v
 
-    python ./train.py --run-name TEST --blur-schedule $1 --train-k-images ${$2:1000}
-}
+# python ./train.py --run-name TEST --blur-schedule $1 --train-k-images ${$2:1000}
