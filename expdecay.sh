b0c2b6d04695867d9634324615b3dac9d1cfa847
Fabrice Normandin <fabrice.normandin@gmail.com>
2019-04-19 17:46:52 -0400

    Put common setup in one place
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- expdecay.sh
+++ expdecay.sh
@@ -2,16 +2,6 @@
 #SBATCH --ntasks-per-node=4
 #SBATCH --gres=gpu:4
 #SBATCH --mem=16G
-#SBATCH --time=4-00:00
-
-source ~/anaconda3/bin/activate
-conda activate tensorflow1
-
+#SBATCH --time=1-00:00
 cd ~/IFT6085/progressive_growing_of_gans
-git checkout master
-git pull
-
-cp -r --no-clobber datasets /Tmp/pichetre/ -v
-
-python ./train.py --blur-schedule EXPDECAY --train-k-images 1000
-
+bash train.sh EXPDECAY
