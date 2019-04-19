64be3368ca57f598bda81f99036d76662119cb20
Fabrice Normandin <fabrice.normandin@gmail.com>
2019-04-19 18:36:31 -0400

    Fixing scripts again
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- expdecay.sh
+++ expdecay.sh
@@ -3,6 +3,15 @@
 #SBATCH --gres=gpu:4
 #SBATCH --mem=16G
 #SBATCH --time=1-00:00
+
 cd ~/IFT6085/progressive_growing_of_gans
-. ./setup.sh
+
+source ~/miniconda3/bin/activate
+conda activate tensorflow1
+
+git checkout master
+git pull
+
+cp -r --no-clobber datasets /Tmp/pichetre/ -v
+
 python ./train.py --blur-schedule EXPDECAY --train-k-images 1000
--- test.sh
+++ test.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 #SBATCH --ntasks-per-node=4
-#SBATCH --gres=gpu:2
+#SBATCH --gres=gpu:4
 #SBATCH --mem=16G
 #SBATCH --time=1-00:00
+
 cd ~/IFT6085/progressive_growing_of_gans
-. ./train.sh
+
+source ~/miniconda3/bin/activate
+conda activate tensorflow1
+
+git checkout master
+git pull
+
+cp -r --no-clobber datasets /Tmp/pichetre/ -v
+
 python ./train.py --run-name TEST --blur-schedule LINEAR --train-k-images 10
